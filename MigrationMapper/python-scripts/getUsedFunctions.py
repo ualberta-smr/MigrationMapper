9f5bfdd31c9172756a63af660dcc3bf47062c43e
Mohayemin <mohayeminul.islam@gmail.com>
2022-09-03 00:12:01 -0600

    fix detection

--- MigrationMapper/python-scripts/getUsedFunctions.py
+++ MigrationMapper/python-scripts/getUsedFunctions.py
@@ -23,32 +23,33 @@ class FunctionCallVisitor(ast.NodeVisitor):
             name = ast_helper.get_function_name(node)
             if name in self.all_lib_functions:
                 func_info = self.all_lib_functions[name]
-                self.called_lib_functions.add(func_info["signature"])
+                self.called_lib_functions.add(func_info["fullName"])
         self.generic_visit(node)
 
 
 if __name__ == '__main__':
 
     sys.argv += [
-        "raven==6.10.0",
-        "../Clone/sipa/sipa/initialization.py",
-        "1",
-        "1000"
+        "raven~=6.2.1",
+        "../Clone/Diffs/1/v47_ea23791cfdc36d614189418a01a57c78859fa5e8/diff_initialization.py_before.java",
+        "177",
+        "24"
     ]
 
     lib_spec, source_path, start_line, lines_count = sys.argv[1:5]
     lib_path = path_helper.get_lib_index_file_path(lib_spec)
-    with open(lib_path) as file:
+    with open(lib_path, encoding="utf8") as file:
         lib_info = json.loads(file.read())
         all_lib_functions = lib_info["functions"]
 
     try:
-        with open(source_path) as source:
+        with open(source_path, encoding="utf8") as source:
             tree = ast.parse(source.read())
         visitor = FunctionCallVisitor(start_line, lines_count, all_lib_functions)
         visitor.visit(tree)
         for funcs in sorted(visitor.called_lib_functions):
             print(funcs)
-    except:
+    except Exception as e:
+        print(e)
         pass
         # don't print anything
